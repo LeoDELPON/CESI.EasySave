83190e1c272be1c805bc959890f89147efbed3a3
LeoDELPON <leo.delponviacesi@gmail.com>
2020-11-25 22:17:29 +0100

    modified BSEasySaveConstructor by adding Save factory()

--- CESI.BS.EasySave/DAL/Work.cs
+++ CESI.BS.EasySave/DAL/Work.cs
@@ -61,9 +61,6 @@ namespace CESI.BS.EasySave.DAL
             RemainingFiles = Convert.ToUInt32(properties[WorkProperties.RemainingFiles]);
             Size = Convert.ToUInt32(properties[WorkProperties.Size]);
             RemainingSize = Convert.ToUInt32(properties[WorkProperties.RemainingSize]);
-
-
-
         }
         public void Perform()
         {
