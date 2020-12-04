c98e9407059823e35de63bb0549a75e4dfce6f14
Marcus BLMNT <marcus.belmont@viacesi.fr>
2020-12-04 17:56:14 +0100

    Language Changé en xaml

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
+using System.Resources;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,22 +17,26 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace WpfApp1
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ResourceDictionary obj;
+        ResourceManager rm = new ResourceManager("fr-FR", Assembly.GetExecutingAssembly());
         public MainWindow()
         {
             InitializeComponent();
             this.Show();
         }
-
+    
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-        
+
         }
 
         private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -38,19 +46,40 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-           
-         
+
+            ChangeLangage(new Uri(@"\Language\en-US.xaml", UriKind.Relative));
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-           
+          
+            ChangeLangage(new Uri(@"\Language\fr-FR.xaml", UriKind.Relative));
         }
+        public void ChangeLangage(Uri dictionnaryUri)
+        {
+            if (String.IsNullOrEmpty(dictionnaryUri.OriginalString) == false)
+            {
+                ResourceDictionary objNewLanguageDictionary = (ResourceDictionary)(Application.LoadComponent(dictionnaryUri));
 
+                if (objNewLanguageDictionary != null)
+                {
+                    this.Resources.MergedDictionaries.Remove(obj);
+                    this.Resources.MergedDictionaries.Add(objNewLanguageDictionary);
+
+                    CultureInfo culture =
+                       new CultureInfo((string)Application.Current.Resources["Culture"]);
+                    Thread.CurrentThread.CurrentCulture = culture;
+                    Thread.CurrentThread.CurrentUICulture = culture;
+                }
+            }
+        }
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-        
+
 
         }
+       
+
     }
 }
+
