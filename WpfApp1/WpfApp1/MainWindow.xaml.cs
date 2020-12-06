d07db2a0d7d7fcc2c8c4d075321a085f9a34a377
Marcus BLMNT <marcus.belmont@viacesi.fr>
2020-12-06 23:20:00 +0100

    Ajout des processus metiers

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
@@ -29,9 +30,10 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-     
+        ProcessusChoosing processusChoosing = new ProcessusChoosing(Process.GetProcesses());
         LanguageSelectionWindow languageSelectionWindow = new LanguageSelectionWindow();
         AddWorkWindow addWorkWindow = new AddWorkWindow();
+        List<string> forbProc = new List<string>();
         
         List<ConfSaver.WorkVar> listWorks = new List<ConfSaver.WorkVar>();
         List<WrkElements> weList = new List<WrkElements>(); 
@@ -53,6 +55,20 @@ namespace WpfApp1
             addExistingWorksToView();
             ChangeLangage(languageSelectionWindow.getLanguagePath());
             languageSelectionWindow.OkBtn.Click += LanguageOkBtn_Click;
+            processusChoosing.OkBtn.Click += pcOkBtn;
+        }
+
+        private void pcOkBtn(object sender, RoutedEventArgs e)
+        {
+            processusChoosing.Hide();
+            forbProc.Clear();
+            foreach(ComboBox cb in processusChoosing.ListCB.Items)
+            {
+                if (cb.SelectedIndex > -1)
+                {
+                    forbProc.Add(cb.SelectedItem.ToString());
+                }
+            }
         }
 
         private void ModifyOkBtn_Click(object sender, RoutedEventArgs e)
@@ -109,7 +125,7 @@ namespace WpfApp1
                 wv.source = addWorkWindow.WorkSourceTB.Text;
                 wv.target = addWorkWindow.WorkTargetTB.Text;
                 wv.typeSave = addWorkWindow.SaveTypeCB.SelectedIndex;
-                bs.AddWork(wv.name, wv.source, wv.target, ((ComboBoxItem)addWorkWindow.SaveTypeCB.SelectedItem).Name);// ajout du travail
+                bs.AddWork(wv.name, wv.source, wv.target, ((ComboBoxItem)addWorkWindow.SaveTypeCB.SelectedItem).Name , new List<string>(),"clé de chiffrage");// ajout du travail
                 WrkElements we = new WrkElements(wv, bs);
                 
             
@@ -126,7 +142,7 @@ namespace WpfApp1
         private void ToWorkList_Click(object sender, RoutedEventArgs e, WrkElements we)
         {
             ToWorkList(we);
-
+            
         }
 
         private void ToWorkList(WrkElements we)
@@ -171,20 +187,14 @@ namespace WpfApp1
             we.inSvdList.MouseDoubleClick += (sender, e) => modifyWorkWindow.DoubleClickOnWorkElement(sender, e, we);
             SaveListLbl.Items.Add(we.inSvdList);
             weList.Add(we);
-            bs.AddWork(we.wv.name, we.wv.source, we.wv.target, SaveTypeMethods.GetSaveTypeFromInt(we.wv.typeSave));
+            bs.AddWork(we.wv.name, we.wv.source, we.wv.target, SaveTypeMethods.GetSaveTypeFromInt(we.wv.typeSave), new List<string>(), "clé de chiffrage");
         }
 
       
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
 
-        }
 
-        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
+  
 
         public void ChangeLangage(Uri dictionnaryUri)
         {
@@ -206,7 +216,7 @@ namespace WpfApp1
                 }
             }
         }
-
+       
         public void launchWorkList()
         {
             Application.Current.Dispatcher.Invoke(() =>
@@ -266,12 +276,27 @@ namespace WpfApp1
                 launchWorkBtn.IsEnabled = true;
             });
         }
-        private void launchWorksButton(object sender, RoutedEventArgs e)            
+        private void launchWorksButton(object sender, RoutedEventArgs e)
         {
-            
+            Process[] aProc;
+           
             Thread worksThreads = new Thread(launchWorkList);
+            if (forbProc.Count == 0)
+            {
+                worksThreads.Start();
+            }
+            else
+            {
+                foreach(string name in forbProc)
+                {
+                    aProc = Process.GetProcessesByName(name);
+                   if (aProc.Length > 0) { 
+                        return;
 
-            worksThreads.Start();
+                    }
+                }
+                worksThreads.Start();
+            }
             
         }
 
@@ -315,6 +340,13 @@ namespace WpfApp1
                 }
             }
         }
+
+        private void CriticalProcessesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            processusChoosing.Show();
+        }
+
+
     }
 }
 
---
+++ WpfApp1/WpfApp1/ProcessusChoosing.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour ProcessusChoosing.xaml
+    /// </summary>
+    public partial class ProcessusChoosing : Window
+    {
+        Process[] listeProcess;
+        public ProcessusChoosing(Process[] listeProcess)
+        {
+            InitializeComponent();
+            Closing += ProcessusChoosing_Closing;
+            this.listeProcess = listeProcess;
+            AddNewCB();   
+          
+            
+        }
+
+        private void ProcessusChoosing_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
+
+        public void AddNewCB()
+        {
+            ComboBox cb = new ComboBox();
+            cb.Width = 165;
+            cb.IsEditable = true;
+            ListCB.Items.Add(cb);
+            foreach (Process process in listeProcess)
+            {
+                cb.Items.Add(process.ProcessName);
+            }
+        }
+        public void RemoveCB()
+        {
+            if (ListCB.Items.Count > 0)
+            {
+                ListCB.Items.Remove(ListCB.Items.GetItemAt(ListCB.Items.Count - 1));
+            }
+        }
+
+
+        private void PlusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddNewCB();
+        }
+
+        private void MinusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            RemoveCB();
+        }
+    }
+}
