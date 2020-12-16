94c6682b17076366a52954b5d509149eb95ead45
Marcus BLMNT <marcus.belmont@viacesi.fr>
2020-12-16 10:25:14 +0100

    le commit

--- CESI.Server.EasySave/Services/PacketHandler.cs
+++ CESI.Server.EasySave/Services/PacketHandler.cs
@@ -1,43 +1,25 @@
-﻿using CESI.BS.EasySave.BS;
-using System;
+﻿using System;
 using System.Net.Sockets;
 
 namespace CESI.Server.EasySave.Services
 {
     public static class PacketHandler
     {
-        public delegate void AbortDelegate();
-        public delegate bool ResumeDelegate();
-        public delegate bool PauseDelegate();
-        public static ResumeDelegate OnResumeSent { get; set; } = DefaultOnResumeSent;
-        public static PauseDelegate OnPauseSent { get; set; } = DefaultOnPauseSent;
-        public static AbortDelegate OnAbortSent { get; set; } = DefaultOnAbortSent;
-
         public static void Handle(byte[] packet, Socket clientSocket)
         {
             Message msg = new Message(packet);
             ActionResultMessage(msg.Text);
+        }
 
-        }
-        public static bool DefaultOnResumeSent() {
-            return true;
-        }
-        public static bool DefaultOnPauseSent() {
-            return true;
-        }
-        public static void DefaultOnAbortSent() { }
         public static void ActionResultMessage(string msg)
         {
             switch (msg)
             {
                 case "STOP":
-                    OnAbortSent();
                     break;
                 case "START":
-                    OnResumeSent();
                     break;
                 case "PAUSE":
-                    OnPauseSent();
                     break;
                 default:
                     break;
